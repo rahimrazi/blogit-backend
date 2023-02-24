bce6930ecdbb7c467e25869efc236fb0f392e694
Razi <12abdulrahim21@gmail.com>
2023-02-24 23:22:51 +0530

    test

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ const server  = app.listen(PORT,console.log(`server is running on port ${PORT}`)
 const io = require("socket.io")(server,{
     pingTimeout:60000,
     cors:{
-        origin:'*',
+        origin: 'https://www.blogit.pizahub.online',
         methods: ["GET", "POST"]
         // origin:["https://blogit.pizahub.online","http://localhost:3000"],
     }
